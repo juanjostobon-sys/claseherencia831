1fe072fe6dd59f3964038f0378558c133a397f9a
tobon <tobon@11.11.20.80>
2026-03-25 21:51:36 -0500

    creacion de la vista en consola y formacion del poliformismo

--- src/main/java/Vista/Herencia831.java
+++ src/main/java/Vista/Herencia831.java
@@ -8,9 +8,33 @@ package Vista;
  *
  * @author tobon
  */
+import modelo.Empleado;
+import modelo.Gerente;
+import controlador.NominaController;
 public class Herencia831 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("aprendiendo herencia");
+        NominaController control = new NominaController();
+        
+        
+        control.registrarEmpleados(new Empleado("Juan Jose",1000));
+        control.registrarEmpleados(new Gerente("juan jose 2",5000,500) );
+        
+        
+        System.out.println("==Listado de pagos==");
+        for(Empleado emp :control.obtenerLista()){
+            
+            
+        System.out.println("Nombre: "+ emp.getNombre());
+        System.out.println("Pago: "+ emp.calcularPago());
+        System.out.println("------------------------------------");
+        System.out.println("===Prueba de sobrecarga===");
+        Empleado busqueda1 = control.buscar("Juan Jose");
+        Empleado busqueda2 = control.buscar(1);
+               
+        System.out.println("===Prueba de sobrecarga busqueda por nombre:  ==="+ (busqueda1 != null? busqueda1.getNombre():"no existe"));
+        System.out.println("===Prueba de sobrecarga busqueda por indice:  ==="+ (busqueda2 != null? busqueda1.getNombre():"no existe"));
+        }
     }
 }
