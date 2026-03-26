b5401eece13e19be3d48734bc0531771408c51f8
tobon <tobon@11.11.20.80>
2026-03-25 19:26:07 -0500

    aprendiendo herencia en java

---
+++ src/main/java/com/mycompany/herencia831/Herencia831.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herencia831;
+
+/**
+ *
+ * @author tobon
+ */
+public class Herencia831 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
